addb1d23cd6179540cf9ef82fdfd2bf613402c9b
BALESUNILKUMARREDDY <NEC0822020@nttf.co.in>
2025-05-19 10:56:09 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,20 +5,20 @@ output "ec2_instance_public_ip" {
 
 output "s3_bucket_name" {
   description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.artifact_bucket.id
+  value       = aws_s3_bucket.artifact_bucket.bucket
 }
 
 output "kubernetes_namespace" {
   description = "Kubernetes namespace created"
-  value       = kubernetes_namespace.app_namespace.metadata[0].name
+  value       = kubernetes_namespace.ml_app_namespace.metadata[0].name
 }
 
 output "kubernetes_deployment_name" {
   description = "Kubernetes deployment name"
-  value       = kubernetes_deployment.flask_app.metadata[0].name
+  value       = kubernetes_deployment.flask_ml_app.metadata[0].name
 }
 
 output "kubernetes_service_name" {
   description = "Kubernetes service name"
-  value       = kubernetes_service.flask_service.metadata[0].name
+  value       = kubernetes_service.flask_ml_service.metadata[0].name
 }
